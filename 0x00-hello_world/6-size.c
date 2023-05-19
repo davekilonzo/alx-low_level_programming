093afe1e2ac6777ede9f20628306b095f31582d5
davekilonzo <davidkilonzo1998@gmail.com>
2023-05-18 18:43:52 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("Size of char: %lu byte(s)\n");
+}
