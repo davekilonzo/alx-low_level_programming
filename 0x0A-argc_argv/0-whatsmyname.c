836746e2310f5a7a3055ee94604789ee7db2ab15
davekilonzo <davidkilonzo1998@gmail.com>
2023-06-17 12:16:33 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - name of the program
+ * @argc: - number of command line arguments
+ * @argv: - ary containing content of command libe arguments
+ *
+ * Return: return 0 ( success0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
