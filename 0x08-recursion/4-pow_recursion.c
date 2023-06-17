008fda0d1d6ca55fef2c63c0505350c339d6553d
davekilonzo <davidkilonzo1998@gmail.com>
2023-06-17 05:40:00 -0700

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main"
+/**
+ * _pow_recursion - calculates the power of a number
+ * @a: the number to calculate its power
+ * @y: number to be raised by
+ *
+ * Return: returns the power of the number
+ */
+int _pow_recursion(int a, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (a * _pow_recursion(a, y - 1));
+	}
+}
