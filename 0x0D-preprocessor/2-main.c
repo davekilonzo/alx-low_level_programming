55e20660697b371cfe33266933b0c4ed39256293
davekilonzo <davidkilonzo1998@gmail.com>
2023-06-19 17:03:38 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints a message to standard output
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
