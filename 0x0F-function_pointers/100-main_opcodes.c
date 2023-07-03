5ccc9e6c0453406b657c4df5c79c95bc508e6de7
davekilonzo <davidkilonzo1998@gmail.com>
2023-07-02 21:03:00 -0700

    100-main_upcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -8,36 +8,29 @@
  *
  * Return: 0 on success, 1 or 2 on error
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-    int bytes, i;
-    unsigned char *main_ptr;
+	int i, n;
+	unsigned char *p;
 
-    if (argc != 2)
-    {
-        printf("Error\n");
-        exit(1);
-    }
-
-    bytes = atoi(argv[1]);
-
-    if (bytes < 0)
-    {
-        printf("Error\n");
-        exit(2);
-    }
-
-    main_ptr = (unsigned char *)main;
-
-    for (i = 0; i < bytes; i++)
-    {
-        printf("%02x", main_ptr[i]);
-        if (i < bytes - 1)
-            printf(" ");
-    }
-
-    printf("\n");
-
-    return (0);
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	p = (unsigned char *)main;
+	for (i = 0; i < n; i++)
+	{
+		printf("%02x", p[i]);
+		if (i < n - 1)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
 }
-
