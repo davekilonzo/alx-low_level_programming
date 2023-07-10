633f952da994ac4dca89dd603284d8269c40fdb1
davekilonzo <davidkilonzo1998@gmail.com>
2023-07-10 03:31:53 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+/**
+ * create_file - creates a file with specified content
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t n_written;
+	mode_t mode = S_IRUSR | S_IWUSR;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		n_written = write(fd, text_content, strlen(text_content));
+		if (n_written == -1)
+			return (-1);
+	}
+	close(fd);
+	return (1);
+}
