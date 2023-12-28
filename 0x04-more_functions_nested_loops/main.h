a04baa5869e908542dd4442a147a5dd19111ea69
davekilonzo <davidkilonzo1998@gmail.com>
2023-12-28 01:11:44 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,11 +1,10 @@
-#ifndef MAIN.H
-#define MAIN.H
-
+#ifndef MAIN_H
+#define MAIN_H
 /**
- * main - header files for all the prototypes
- * void - void prototypes
- * int - int prototypes(int)
- */
+*main - header file for protoypes
+*void protoypes (void)
+*int protoypes (int)
+*/
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
