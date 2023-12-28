59c7cfd8a62a3478a7284b1e5c0f991ef804e037
davekilonzo <davidkilonzo1998@gmail.com>
2023-12-28 01:07:27 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - checks the uppercase characters
+ * @c: the character to check
+ *
+ * Return: 1 if c is upperclass, 0 otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN.H
+#define MAIN.H
+
+/**
+ * main - header files for all the prototypes
+ * void - void prototypes
+ * int - int prototypes(int)
+ */
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+
+#endif /* MAIN_H */
